1976c145496d939bfad0e3903d0101ed03933a9e
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-05-31 14:53:17 +0800

    Initial Pizza Menu files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pizza-menu
+# Pizza Menu
+
+This project was made with Jonas Schmedtmann's The Ultimate React Course 2024
+
+## Features
+
+- Menu created using list.
+
+## Screenshot
+
+![pizza-menu](https://github.com/LuisCabantac/pizza-menu/assets/151472373/08a98112-a79b-4f76-95ff-6d8d2d381af6)
---
+++ src/components/Menu.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { pizzaData } from "../App";
+import Pizza from "./Pizza";
+
+export default function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+
+      {numPizzas > 0 ? (
+        <React.Fragment>
+          <p>
+            Authentic Italian Cuisine. 6 creative dishes to choose from. All
+            from out stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
+      ) : (
+        <p>We're still working on our menu. Please come back later</p>
+      )}
+    </main>
+  );
+}
